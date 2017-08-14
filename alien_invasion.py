898c596b6bfe17a4a19665843233feffe65a9c4c
shinnnne <1012304054@qq.com>
2017-08-14 16:13:06 +0800

    add alien_invasion.py

---
+++ alien_invasion.py
@@ -0,0 +1,27 @@
+import sys
+
+import pygame
+
+def run_game():
+	
+	pygame.init()
+	screen = pygame.display.set_mode(1200, 800)
+	pygame.display.set_caption("Alien Invasion")
+	
+	# 设置背景色
+	bg_color = (230, 230, 230)
+	
+	# 开始游戏的主循环
+	while True:
+		#监听鼠标和键盘事件
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+		#每次循环时都重绘屏幕
+		screen.fill(bg_color)
+		
+		# 让最近绘制的屏幕可见	
+		pygame.display.flip()
+
+run_game()
+			
